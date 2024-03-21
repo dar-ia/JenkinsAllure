18204bbca82f9f85bbcd8a625019ca2d3769bffe
dar-ia <iriryt@gmail.com>
2024-03-21 19:44:17 +0400

    Not used imports deleted

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,18 +1,13 @@
 package tests;
-
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.util.Map;
-
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class TestBase {
     @BeforeAll
